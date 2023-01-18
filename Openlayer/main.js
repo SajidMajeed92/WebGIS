a29380a869bb451ed1120477f40216bee823a9fa
SajidMajeed92 <sajidmajeed.ist@gmail.com>
2023-01-18 15:59:57 +0500

    first commit

---
+++ Openlayer/main.js
@@ -0,0 +1,18 @@
+// Map Initilization 
+var map = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    target: 'map1',
+    controls: ol.control.defaults({
+      attributionOptions: {
+        collapsible: false
+      }
+    }),
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 2
+    })
+  });
